b9ba70db8e6a3e8439e27bb6e645b57ca609f7ad
vjha23 <51393417+vjha23@users.noreply.github.com>
2019-06-10 08:38:01 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,15 @@
+number=[1,2,3,4,5,66,22,2,6,0,9]
+x=[]
+y=[]
+print('Number that are greater than 5 :')
+for i in number:
+ if i>5:
+  print(i)
+   x.append(i)
+print('number that are less than and equal 2 : ')
+for i in number:
+    if i<=2:
+        print(i)
+         y.append
+print('list 1 is :',x)
+print('list 2 is :',y)
